9225b8b9e4403b2338c4dfd8d78314c260068fab
mjp0997 <mjp0997@gmail.com>
2023-02-28 21:37:09 -0400

    Función para obtener el precio del dólar
    La función retorna el precio del dólar según el BCV

---
+++ src/helpers/exchange.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const https = require('https');
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+const cheerio = require('cheerio');
+
+
+
+/**
+ * Función para obtener el precio del dólar tasa BCV
+ * @async
+ * @returns {Promise<number|null>} El precio del dólar o null en caso de error
+ */
+const dolarExchange = async () => {
+   try {
+      const response = await axios.get('https://www.bcv.org.ve', {httpsAgent});
+   
+      const parse = cheerio.load(response.data);
+
+      const dolarRaw = parse('#dolar').text();
+
+      const dolar = Number(dolarRaw.replace('USD', '').trim().replace(',', '.'));
+
+      return dolar;
+   } catch (error) {
+      console.log(error);
+      return null;
+   }
+}
+
+
+
+module.exports = {
+   dolarExchange
+}
