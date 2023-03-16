8afd235883e25afe2fd6fb628d6c2541c7f90f75
mjp0997 <mjp0997@gmail.com>
2023-03-16 13:03:25 -0400

    Agregados permisos faltantes

--- src/database/seeders/20221226031102-create-permissions.js
+++ src/database/seeders/20221226031102-create-permissions.js
@@ -3,38 +3,21 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-
     const permissions = [
-      {
-        name: 'users',
-        showName: 'usuarios',
-        isPublic: true
-      },
-      {
-        name: 'roles',
-        showName: 'roles',
-        isPublic: true
-      },
-      {
-        name: 'companies',
-        showName: 'clientes',
-        isPublic: false
-      },
-      {
-        name: 'cities',
-        showName: 'ciudades',
-        isPublic: false
-      },
-      {
-        name: 'states',
-        showName: 'estados',
-        isPublic: false
-      },
-      {
-        name: 'countries',
-        showName: 'países',
-        isPublic: false
-      }
+      { name: 'users', showName: 'usuarios', isPublic: true },
+      { name: 'roles', showName: 'roles', isPublic: true },
+      { name: 'companies', showName: 'clientes', isPublic: false },
+      { name: 'cities', showName: 'ciudades', isPublic: false },
+      { name: 'states', showName: 'estados', isPublic: false },
+      { name: 'countries', showName: 'países', isPublic: false },
+      { name: 'products', showName: 'productos', isPublic: true },
+      { name: 'receptions', showName: 'envíos', isPublic: true },
+      { name: 'receivers', showName: 'destinatarios', isPublic: true },
+      { name: 'batches', showName: 'lotes', isPublic: false },
+      { name: 'dispatches', showName: 'despachos', isPublic: true },
+      { name: 'config', showName: 'configuraciones', isPublic: false },
+      { name: 'payment-methods', showName: 'métodos de pago', isPublic: false },
+      { name: 'payments', showName: 'pagos', isPublic: true },
     ];
 
     await queryInterface.bulkInsert('permissions', permissions.map(per => ({
