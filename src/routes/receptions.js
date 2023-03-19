c2a9372139baed77046f8d5b760f60942eb65660
mjp0997 <mjp0997@gmail.com>
2023-03-19 17:29:59 -0400

    HOTFIX: middlewares
    Agregando validaciones de los atributos usados para filtrar en el endpoint findAll

--- src/routes/receptions.js
+++ src/routes/receptions.js
@@ -22,7 +22,9 @@ const router = Router();
 router.get('/', [
    validateJWT,
 
-   query('name', 'El nombre debe tener un formato string').optional().isString(),
+   query('date', 'La fecha debe tener un formato válido').optional().isDate(),
+   query('companyId', 'El id es inválido').optional().isInt({gt: 0}),
+   query('userId', 'El id es inválido').optional().isInt({gt: 0}),
 
    query('limit', 'El límite de documentos debe ser un entero mayor a cero').optional().isInt({gt: 0}),
    query('skip', 'La cantidad de documentos a omitir debe ser un entero mayor a cero').optional().isInt({min: 0}),
