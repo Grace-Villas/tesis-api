a4ff7bbe74a27e01fbe09378f32a7768a911baf2
Mario José Pineda <72577859+mjp0997@users.noreply.github.com>
2023-03-26 21:20:11 -0400

    HOTFIX

--- src/middlewares/product-express.js
+++ src/middlewares/product-express.js
@@ -4,21 +4,21 @@ const { Product } = require('../database/models');
 
 /**
  * Función para validar la existencia de un producto dentro de la colección productos, dado su id
- * @param {integer} stateId id del estado
+ * @param {integer} productId id del producto
  * @returns {boolean}
  */
-const validateProductId = async (countryId) => {
+const validateProductId = async (productId) => {
    try {
-      const product = await Product.findByPk(countryId);
+      const product = await Product.findByPk(productId);
 
       if (!product) {
-         throw new Error(`El id: ${countryId} no se encuentra en la base de datos`);
+         throw new Error(`El id: ${productId} no se encuentra en la base de datos`);
       }
 
       return true;
    } catch (error) {
       console.log(error);
-      throw new Error(`El id: ${countryId} no se encuentra en la base de datos`);
+      throw new Error(`El id: ${productId} no se encuentra en la base de datos`);
    }
 }
 
