728959d8595ebb6b37dfd3a3fe38ca5a5af9bcec
Mario José Pineda <72577859+mjp0997@users.noreply.github.com>
2023-03-29 00:59:06 -0400

    Endpoints de status de lotes

---
+++ src/routes/batch-statuses.js
@@ -0,0 +1,34 @@
+const { Router } = require('express');
+const { query } = require('express-validator');
+
+const { validateJWT } = require('../middlewares/validate-jwt');
+const { validateFields } = require('../middlewares/validate-fields');
+
+const {
+   findAll,
+} = require('../controllers/batch-statuses');
+
+
+
+const router = Router();
+
+// Rutas
+
+// Listar tipos de pagos registrados
+router.get('/', [
+   validateJWT,
+
+   query('steps', 'El filtro de pasos debe ser un booleano').optional().isBoolean(),
+
+   query('limit', 'El límite de documentos debe ser un entero mayor a cero').optional().isInt({gt: 0}),
+   query('skip', 'La cantidad de documentos a omitir debe ser un entero mayor a cero').optional().isInt({min: 0}),
+   validateFields
+], findAll);
+
+
+
+// Exports
+module.exports = {
+   name: 'batch-statuses',
+   router
+};
