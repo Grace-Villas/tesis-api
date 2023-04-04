903153d33d6eae3428786b2b86beb5f16a719a2f
Mario José Pineda <72577859+mjp0997@users.noreply.github.com>
2023-04-03 22:58:23 -0400

    Agregada documentación

--- src/helpers/batches.js
+++ src/helpers/batches.js
@@ -2,12 +2,16 @@ const { Batch, BatchStatus, Dispatch, DispatchStatus } = require('../database/mo
 
 
 
+/**
+ * Verifica el estado del lote, si no posee mas despachos en tránsito lo marca como finalizado
+ * @param {number} batchId id del lote a verificar
+ */
 const updateBatchIfFullyDelivered = async (batchId) => {
    const status = await DispatchStatus.findOne({
       where: { name: 'embarcado' }
    })
 
-   const batch = await Batch.findByPk(1, {
+   const batch = await Batch.findByPk(batchId, {
       include: {
          model: Dispatch,
          as: 'dispatches',
